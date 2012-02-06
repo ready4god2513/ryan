b3519a9a51f223a7e2482133987bf7f097485a12
Brandon Hansen <brandonh@ibethel.org>
2012-02-06 10:03:45 -0800

    Animation

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,4 +1,23 @@
 $(function(){
 
+	
+	
+	$("#registries").addClass("animated bounceInLeft").css("display", "block");
+	
+	setTimeout(function(){
+		$("#rsvp").addClass("animated bounceInUp").css("display", "block");
+	}, 200);
+	
+	setTimeout(function(){
+		$("#reception-details").addClass("animated bounceInUp").css("display", "block");
+	}, 600);
+	
+	setTimeout(function(){
+		$("#ryan-krystyna-closeup").addClass("animated bounceInRight").css("display", "block");
+	}, 1000);
+	
+	setTimeout(function(){
+		$("#directions").addClass("animated bounceInDown").css("display", "block");
+	}, 1200);
 
 });
